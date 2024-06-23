5eb505d6421aa217cf807aa39c74f912861f916b
Kunal prajapati <106148593+Kunalprajapati1@users.noreply.github.com>
2024-06-23 16:54:10 +0800

    upp

--- src/components/PersonDetails.js
+++ src/components/PersonDetails.js
@@ -1,274 +1,4 @@
 
-// import React, { useState, useEffect } from 'react';
-// import {
-//   StyleSheet,
-//   Text,
-//   View,
-//   ImageBackground,
-//   ScrollView,
-//   TouchableOpacity,
-  
-// } from 'react-native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import firestore from '@react-native-firebase/firestore';
-
-// const PersonDetails = ({ route, navigation }) => {
-//   const { params } = route;
-//   const personData = params ? params.personData : null;
-//   const profileForDisplayOrder = params ? params.profileForDisplayOrder : null;
-//   const [emailExists, setEmailExists] = useState(false);
-
-//   const [showContactDetails, setShowContactDetails] = useState(false);
-//   const [userEmail, setUserEmail] = useState('');
-
-//   useEffect(() => {
-//     const fetchUserEmail = async () => {
-//       try {
-//         const email = await AsyncStorage.getItem('userEmail');
-//         setUserEmail(email);
-//         checkEmailInDatabase(email);
-
-//       } catch (error) {
-//         console.error('Error fetching user email from AsyncStorage:', error);
-//       }
-//     };
-
-//     fetchUserEmail();
-//   }, []);
-
-//   const checkEmailInDatabase = async (email) => {
-//     try {
-//       const snapshot = await firestore().collection('Preminum').where('email', '==', email).get();
-//       if (snapshot.empty) {
-//         console.log('Email not found in database.');
-//         setEmailExists(false);
-//       } else {
-//         console.log('Email found in database.');
-//         setEmailExists(true);
-//       }
-//     } catch (error) {
-//       console.error('Error checking email in database:', error);
-//     }
-//   };
-
-//   if (!personData || !profileForDisplayOrder) {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.errorText}>
-//           Error: Person data or display order not available.
-//         </Text>
-//       </View>
-//     );
-//   }
-//   const handleToggleDetails = () => {
-//     if (emailExists) {
-//       setShowContactDetails(!showContactDetails);
-//     } else {
-//       navigation.navigate('Premium'); // Navigate to Home page if email not found
-//     }
-//   };
-//   const navigateToChat = () => {
-//     navigation.navigate('Chat', { personData });
-//   };
-
-//   const navigateToSearch = () => {
-//     navigation.navigate('Search');
-//   };
-
-//   const capitalizeFirstLetter = string => {
-//     return string.charAt(0).toUpperCase() + string.slice(1);
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <ImageBackground
-//         source={
-//           personData.photos && personData.photos.length > 0
-//             ? { uri: personData.photos[0] }
-//             : require('../assets/app_images/user.png')
-//         }
-//         style={styles.backgroundImage}>
-//         <ScrollView contentContainerStyle={styles.overlayContainer}>
-//           <TouchableOpacity
-//             style={styles.backButton}
-//             onPress={() => {
-//               navigation.navigate('Land');
-//             }}>
-//             {/* Your back button content */}
-//           </TouchableOpacity>
-
-//           {personData && (
-//             <View style={styles.postContainer}>
-//               <Text style={styles.nameText}>
-//                 {personData.firstName} {personData.lastName}
-//               </Text>
-//               <View style={styles.detailsContainer}>
-//                 {profileForDisplayOrder.map(key => (
-//                   <View key={key} style={styles.columnContainer}>
-//                     <Text
-//                       style={[
-//                         styles.fieldText,
-//                         key === 'firstName' ||
-//                         key === 'lastName' ||
-//                         key === 'Gender' ||
-//                         key === 'dateOfBirth' ||
-//                         key === 'diet' ||
-//                         key === 'height' ||
-//                         key === 'collegeName' ||
-//                         key === 'maritalStatus' ||
-//                         key === 'qualification' ||
-//                         key === 'selectedState' ||
-//                         key === 'city' ||
-//                         key === 'Option' ||
-//                         key === 'selectedReligion' ||
-//                         key === 'Community' ||
-//                         key === 'workAs' ||
-//                         key === 'workAsOtherDetails' ||
-//                         key === 'workWith' ||
-//                         key === 'workWithOtherDetails' ||
-//                         key === 'email' ||
-//                         key === 'mobileNumber'
-//                           ? { color: '#e88a8a' }
-//                           : null,
-//                       ]}
-//                     >
-//                       {capitalizeFirstLetter(key)}:
-//                     </Text>
-//                     <Text style={styles.valueText}>
-//                       {showContactDetails || (key !== 'email' && key !== 'mobileNumber')
-//                         ? personData[key] || 'N/A'
-//                         : '********'}
-//                     </Text>
-//                     <View style={styles.line} />
-//                   </View>
-//                 ))}
-//               </View>
-           
-//               <TouchableOpacity
-//                 onPress={handleToggleDetails} // Updated onPress handler
-//                 style={styles.toggleButton}>
-//                 <Text style={styles.toggleButtonText}>
-//                   {showContactDetails ? 'Hide' : 'Show'} Contact Details
-//                 </Text>
-//               </TouchableOpacity>
-           
-//               <TouchableOpacity
-//                 onPress={navigateToSearch}
-//                 style={styles.chatButton}>
-//                 <Text style={styles.chatButtonText}>
-//                 See more Photos
-//                 </Text>
-//               </TouchableOpacity>
-//             </View>
-//           )}
-//         </ScrollView>
-//       </ImageBackground>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#e88a8a',
-//   },
-//   backgroundImage: {
-//     flex: 1,
-//     width: '100%',
-//     height: '100%',
-//     resizeMode: 'cover',
-//   },
-//   overlayContainer: {
-//     flexGrow: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     padding: 16,
-//   },
-//   backButton: {
-//     position: 'absolute',
-//     top: 20,
-//     left: 20,
-//     zIndex: 1,
-//   },
-//   postContainer: {
-//     backgroundColor: 'rgba(0, 0, 0, 0.768)',
-//     borderRadius: 10,
-//     padding: 40,
-//     marginTop: 20,
-//   },
-//   nameText: {
-//     fontFamily: 'DMSerifDisplay-Regular',
-//     fontSize: 32,
-//     color: '#e05654',
-//     marginBottom: 10,
-//   },
-//   detailsContainer: {
-//     flexDirection: 'row',
-//     flexWrap: 'wrap',
-//   },
-//   columnContainer: {
-//     flexDirection: 'column',
-//     alignItems: 'flex-start',
-//     marginRight: 20,
-//     marginBottom: 15,
-//   },
-//   fieldText: {
-//     fontFamily: 'Montserrat-Regular',
-//     fontSize: 17,
-//     lineHeight: 45,
-//     marginBottom: 5,
-//   },
-//   valueText: {
-//     fontFamily: 'Montserrat-Regular',
-//     color: '#e2d0d0',
-//     fontSize: 19,
-//     lineHeight: 25,
-//   },
-//   line: {
-//     borderBottomWidth: 1,
-//     borderBottomColor: '#e2d0d0',
-//     width: '100%',
-//     marginBottom: 5,
-//   },
-//   chatButton: {
-//     backgroundColor: '#e05654',
-//     padding: 10,
-//     borderRadius: 35,
-//     marginTop: 10,
-//   },
-//   chatButtonText: {
-//     color: 'white',
-//     textAlign: 'center',
-//     fontFamily: 'Montserrat-Regular',
-//   },
-//   errorText: {
-//     color: 'white',
-//     fontSize: 18,
-//     textAlign: 'center',
-//     fontFamily: 'Montserrat-Regular',
-//   },
-//   toggleButton: {
-//     backgroundColor: '#e05654',
-//     padding: 10,
-//     borderRadius: 35,
-//     marginTop: 10,
-//   },
-//   toggleButtonText: {
-//     color: 'white',
-//     textAlign: 'center',
-//     fontFamily: 'Montserrat-Regular',
-//   },
-// });
-
-// export default PersonDetails;
-
-
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
@@ -277,7 +7,7 @@ import {
   ImageBackground,
   ScrollView,
   TouchableOpacity,
-  ActivityIndicator,
+  
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import firestore from '@react-native-firebase/firestore';
@@ -287,22 +17,19 @@ const PersonDetails = ({ route, navigation }) => {
   const personData = params ? params.personData : null;
   const profileForDisplayOrder = params ? params.profileForDisplayOrder : null;
   const [emailExists, setEmailExists] = useState(false);
+
   const [showContactDetails, setShowContactDetails] = useState(false);
   const [userEmail, setUserEmail] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchUserEmail = async () => {
       try {
         const email = await AsyncStorage.getItem('userEmail');
         setUserEmail(email);
-        await checkEmailInDatabase(email);
+        checkEmailInDatabase(email);
+
       } catch (error) {
         console.error('Error fetching user email from AsyncStorage:', error);
-        setError('Error fetching user email.');
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -321,7 +48,6 @@ const PersonDetails = ({ route, navigation }) => {
       }
     } catch (error) {
       console.error('Error checking email in database:', error);
-      setError('Error checking email in database.');
     }
   };
 
@@ -334,7 +60,6 @@ const PersonDetails = ({ route, navigation }) => {
       </View>
     );
   }
-
   const handleToggleDetails = () => {
     if (emailExists) {
       setShowContactDetails(!showContactDetails);
@@ -342,7 +67,6 @@ const PersonDetails = ({ route, navigation }) => {
       navigation.navigate('Premium'); // Navigate to Home page if email not found
     }
   };
-
   const navigateToChat = () => {
     navigation.navigate('Chat', { personData });
   };
@@ -351,29 +75,9 @@ const PersonDetails = ({ route, navigation }) => {
     navigation.navigate('Search');
   };
 
-  const navigateToConnected = () => {
-    navigation.navigate('Connected', { personData });
-  };
-
-  const capitalizeFirstLetter = (string) => {
+  const capitalizeFirstLetter = string => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator size="large" color="#e88a8a" />
-      </View>
-    );
-  }
-
-  if (error) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.errorText}>{error}</Text>
-      </View>
-    );
-  }
 
   return (
     <View style={styles.container}>
@@ -383,16 +87,14 @@ const PersonDetails = ({ route, navigation }) => {
             ? { uri: personData.photos[0] }
             : require('../assets/app_images/user.png')
         }
-        style={styles.backgroundImage}
-      >
+        style={styles.backgroundImage}>
         <ScrollView contentContainerStyle={styles.overlayContainer}>
           <TouchableOpacity
             style={styles.backButton}
             onPress={() => {
               navigation.navigate('Land');
-            }}
-          >
-            {/* <Text style={styles.backButtonText}>Back</Text> Wrap "Back" text in a <Text> component */}
+            }}>
+            {/* Your back button content */}
           </TouchableOpacity>
 
           {personData && (
@@ -401,7 +103,7 @@ const PersonDetails = ({ route, navigation }) => {
                 {personData.firstName} {personData.lastName}
               </Text>
               <View style={styles.detailsContainer}>
-                {profileForDisplayOrder.map((key) => (
+                {profileForDisplayOrder.map(key => (
                   <View key={key} style={styles.columnContainer}>
                     <Text
                       style={[
@@ -441,19 +143,21 @@ const PersonDetails = ({ route, navigation }) => {
                   </View>
                 ))}
               </View>
-
-              <TouchableOpacity onPress={handleToggleDetails} style={styles.toggleButton}>
+           
+              <TouchableOpacity
+                onPress={handleToggleDetails} // Updated onPress handler
+                style={styles.toggleButton}>
                 <Text style={styles.toggleButtonText}>
                   {showContactDetails ? 'Hide' : 'Show'} Contact Details
                 </Text>
               </TouchableOpacity>
-
-              <TouchableOpacity onPress={navigateToSearch} style={styles.chatButton}>
-                <Text style={styles.chatButtonText}>See more Photos</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity onPress={navigateToConnected} style={styles.connectButton}>
-                <Text style={styles.connectButtonText}>Connect</Text>
+           
+              <TouchableOpacity
+                onPress={navigateToSearch}
+                style={styles.chatButton}>
+                <Text style={styles.chatButtonText}>
+                See more Photos
+                </Text>
               </TouchableOpacity>
             </View>
           )}
@@ -485,11 +189,6 @@ const styles = StyleSheet.create({
     top: 20,
     left: 20,
     zIndex: 1,
-  },
-  backButtonText: {
-    color: 'white',
-    fontSize: 16,
-    fontFamily: 'Montserrat-Regular',
   },
   postContainer: {
     backgroundColor: 'rgba(0, 0, 0, 0.768)',
@@ -538,17 +237,6 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   chatButtonText: {
-    color: 'white',
-    textAlign: 'center',
-    fontFamily: 'Montserrat-Regular',
-  },
-  connectButton: {
-    backgroundColor: '#34a853',
-    padding: 10,
-    borderRadius: 35,
-    marginTop: 10,
-  },
-  connectButtonText: {
     color: 'white',
     textAlign: 'center',
     fontFamily: 'Montserrat-Regular',
